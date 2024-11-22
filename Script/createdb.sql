55012c3270b896d3249121a1f9ab80c75e1fd8cd
Myy1112 <120002840+Myy1112@users.noreply.github.com>
2024-11-22 23:15:22 +0700

    Update db phân hệ Khách Hàng

--- Script/createdb.sql
+++ Script/createdb.sql
@@ -2,6 +2,36 @@
 
 -- phân hệ chi nhánh
 -- phân hệ khách hàng
+
+CREATE TABLE Ban (
+    MaSoBan INT PRIMARY KEY,
+    MaChiNhanh INT,
+    TrangThai BIT CHECK (TrangThai IN (0, 1)),  -- 0: Trống, 1: Đang sử dụng
+    SucChua INT CHECK (SucChua > 0),
+    FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh)
+);
+
+CREATE TABLE DatCho (
+    MaSoBan INT,
+    MaChiNhanh INT,
+    MaDatTruoc INT,
+    PRIMARY KEY (MaSoBan, MaChiNhanh, MaDatTruoc),
+    FOREIGN KEY (MaSoBan, MaChiNhanh) REFERENCES Ban(MaSoBan, MaChiNhanh),
+    FOREIGN KEY (MaDatTruoc) REFERENCES DatTruoc(MaDatTruoc)
+);
+
+
+CREATE TABLE DatTruoc (
+    MaDatTruoc INT PRIMARY KEY,
+    MaKhachHang INT,
+    MaChiNhanh INT,
+    SoLuongKhach INT CHECK (SoLuongKhach <= (SELECT SucChua FROM Ban WHERE MaSoBan = DatTruoc.MaSoBan)),
+    GioDen TIME,
+    GhiChu TEXT,
+    FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
+    FOREIGN KEY (MaChiNhanh) REFERENCES ChiNhanh(MaChiNhanh)
+);
+
 -- phân hệ nhân viên
 
 
