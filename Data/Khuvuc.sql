f72ff43176528fbb4212576b8c9f849e4bc562f0
huyen12112k4 <128444657+huyen12112k4@users.noreply.github.com>
2024-11-29 02:02:29 +0700

    Update Script

---
+++ Data/Khuvuc.sql
@@ -0,0 +1,6 @@
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (1, N'Hồ Chí Minh');
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (2, N'Hà Nội');
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (3, N'Đà Nẵng');
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (4, N'Hải Phòng');
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (5, N'Cần Thơ');
+INSERT INTO KhuVuc (MaKhuVuc, TenKhuVuc) VALUES (6, N'Vũng Tàu');
---
+++ Data/Mucthucdon.sql
@@ -0,0 +1,8 @@
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (1, N'Khai vị');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (2, N'Sashimi combo');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (3, N'Nigiri');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (4, N'Tempura');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (5, N'Udon');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (6, N'Hotpot (lẩu Nhật)');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (7, N'Lunch set');
+INSERT INTO MucThucDon (MaMuc, TenMuc) VALUES (8, N'Các món nước');
