233887109e7884f09ca6afe061ece5c3bc5829c7
Myy1112 <120002840+Myy1112@users.noreply.github.com>
2024-12-11 13:51:29 +0700

    Update Index

--- Script/index.sql
+++ Script/index.sql
@@ -3,6 +3,10 @@
 
 -- phân hệ chi nhánh
 -- phân hệ khách hàng
+CREATE INDEX idx_PhieuDatMon on PhieuDatMon(MaChiNhanh,MaKhachHang)
+CREATE INDEX idx_ChiTietPhieu on ChiTietPhieu(MaMon)
+CREATE INDEX idx_HoaDon on HoaDon(NgayLap)
+
 CREATE INDEX idx_DatTruoc on DatTruoc(NgayDat, SoLuongKhach)
 CREATE INDEX idx_ThongTinTruyCap on ThongTinTruyCap(ThoiGianTruyCap, ThoiDiemTruyCap)
 -- phân hệ nhân viên
