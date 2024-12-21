4e3b1ebfc36d1203f8a0a08eaa65aba233b9042f
huyen12112k4 <128444657+huyen12112k4@users.noreply.github.com>
2024-12-21 23:23:15 +0700

    Chốt db

--- Code generate/khachhang.py
+++ Code generate/khachhang.py
@@ -1,5 +1,4 @@
 import random
-
 # Function to generate random Vietnamese names
 def generate_name():
     first_names = ["Nguyen", "Tran", "Le", "Pham", "Hoang", "Phan", "Vu", "Dang", "Do", "Bui"]
@@ -38,3 +37,22 @@ with open(output_file, "w", encoding="utf-8") as file:
         )
 
 print(f"File '{output_file}' has been created with 100,000 records.")
+
+output_file = "insert_ban.sql"
+
+# Mở file để ghi dữ liệu
+with open(output_file, "w", encoding="utf-8") as file:
+    for chi_nhanh in range(1, 16):  # Mã chi nhánh từ 1 đến 15
+        for ban in range(1, 41):  # Mã số bàn từ 1 đến 40
+            ma_so_ban = ban
+            ma_chi_nhanh = chi_nhanh
+            trang_thai = 0  # Trạng thái mặc định là 0
+            suc_chua = random.randint(2, 20)  # Sức chứa từ 2 đến 20
+
+            # Tạo câu lệnh SQL
+            sql = f"INSERT INTO Ban (MaSoBan, MaChiNhanh, TrangThai, SucChua) VALUES ({ma_so_ban}, {ma_chi_nhanh}, {trang_thai}, {suc_chua});"
+
+            # Ghi vào file
+            file.write(sql + "\n")
+
+print(f"File '{output_file}' đã được tạo thành công với các câu lệnh INSERT.")
