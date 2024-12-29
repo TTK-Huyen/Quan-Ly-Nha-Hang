3deeb71e6f45007d78635d2e5d7855ff06d91f97
huyen12112k4 <128444657+huyen12112k4@users.noreply.github.com>
2024-12-30 03:19:53 +0700

    Fix some code

--- Data/Mucthucdon.sql
+++
@@ -1,9 +0,0 @@
--- Insert statements for MucThucDon
-INSERT INTO MucThucDon (TenMuc) VALUES ('Khai Vị');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Sashimi Combo');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Nigiri');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Tempura');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Udon');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Hotpot');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Lunch Set');
-INSERT INTO MucThucDon (TenMuc) VALUES ('Món Nước');
