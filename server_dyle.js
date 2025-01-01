3848b16fe207719072bf11a4ca8f94df8165e1a8
Vy <lnyvy22@clc.fitus.edu.vn>
2025-01-01 14:20:55 +0700

    test fetch

--- server_dyle.js
+++ server_dyle.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-
+//test
 // Phục vụ file tĩnh (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
